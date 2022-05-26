0338fc48ac9c68d642161ac1558516dbbe53123f
engWafula <wafulaallan5@gmail.com>
2022-05-26 12:23:30 +0300

    connection to mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import {ApolloServer} from "apollo-server-express";
-import { schema } from "./graphql";
+import {resolvers,typeDefs,} from "./graphql";
 
 const startServer= async()=>{
 
@@ -8,7 +8,7 @@ const app =express();
 const port =9000;
 
 
-const server = new ApolloServer({schema});
+const server = new ApolloServer({typeDefs,resolvers});
 
 await server.start();
 
